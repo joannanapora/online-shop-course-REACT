5b6757a432858502cef1a170165c09b990ebdf6c
porczi <porczi@MacBook-Air.local>
2020-11-16 15:30:09 +0000

    added header component and links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component.jsx';
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
