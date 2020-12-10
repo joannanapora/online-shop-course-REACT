08cb4666babb32ac15b8427445ae707625101fb6
porczi <porczi@MacBook-Air.local>
2020-12-10 15:47:09 +0000

    added redux-persist on checkout page

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from 'redux';
-import userReducer from './user/user.reducer.js'
-import cartReducer from './cart/cart.reducer.js'
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+import cartReducer from './cart/cart.reducer.js';
+import userReducer from './user/user.reducer.js';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
